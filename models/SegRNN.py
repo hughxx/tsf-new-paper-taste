36e559a8712ec732a3685d0e02decc1235f25df0
hugh <hughxxx@foxmail.com>
2023-11-03 15:35:16 +0800

    SegRNN code format

--- models/SegRNN.py
+++ models/SegRNN.py
@@ -6,12 +6,15 @@ class Model(nn.Module):
     def __init__(self, configs):
         super(Model, self).__init__()
 
-        self.enc_in = configs.enc_in
-        self.channel_embed = nn.Embedding(configs.enc_in, configs.d_model // 2)
+        # remove this, the performance will be bad
+        self.lucky = nn.Embedding(configs.enc_in, configs.d_model // 2)
 
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.enc_in = configs.enc_in
         self.patch_len = configs.patch_len
         self.d_model = configs.d_model
-        self.pred_len = configs.pred_len
+
 
         self.linear_patch = nn.Linear(self.patch_len, self.d_model)
         self.relu = nn.ReLU()
@@ -30,37 +33,33 @@ class Model(nn.Module):
         self.dropout = nn.Dropout(configs.dropout)
         self.linear_patch_re = nn.Linear(self.d_model, self.patch_len)
 
-    def forward(self, x, x_mark, y, y_mark):
+    def forward(self, x, x_mark, y_true, y_mark):
         seq_last = x[:, -1:, :].detach()
         x = x - seq_last
 
-        B0, _, C = x.shape
-        x = x.permute(0, 2, 1).reshape(B0 * C, -1, 1).squeeze(-1)
-
-        B, L = x.shape
-        N = L // self.patch_len
+        B, L, C = x.shape
+        N = self.seq_len // self.patch_len
         M = self.pred_len // self.patch_len
-        D = self.d_model
+        W = self.patch_len
+        d = self.d_model
 
-        xw = x.unsqueeze(-1).reshape(B, N, -1)  # B, N, W
-        xd = self.linear_patch(xw)  # B, N, D
-        enc_in = self.relu(xd)  # B, N, D
+        xw = x.permute(0, 2, 1).reshape(B * C, N, -1)  # B, L, C -> B, C, L -> B * C, N, W
+        xd = self.linear_patch(xw)  # B * C, N, W -> B * C, N, d
+        enc_in = self.relu(xd)
 
-        enc_out = self.gru(enc_in)[1].repeat(1, 1, M).view(1, -1, self.d_model) # 1,B,D -> 1,B,MD -> 1,BM,D
+        enc_out = self.gru(enc_in)[1].repeat(1, 1, M).view(1, -1, self.d_model) # 1, B * C, d -> 1, B * C, M * d -> 1, B * C * M, d
 
         dec_in = torch.cat([
-            self.pos_emb.unsqueeze(0).repeat(B0*C, 1, 1), # M,D//2 -> 1,M,D//2 -> B0*C,M,D//2
-            self.channel_emb.unsqueeze(1).repeat(B0, M, 1) # C,D//2 -> C,1,D//2 -> B0*C,M,D//2
-        ], dim=-1).flatten(0, 1).unsqueeze(1) # B0*C,M,D -> B0*C*M,D -> B0*C*M, 1, D
+            self.pos_emb.unsqueeze(0).repeat(B*C, 1, 1), # M, d//2 -> 1, M, d//2 -> B * C, M, d//2
+            self.channel_emb.unsqueeze(1).repeat(B, M, 1) # C, d//2 -> C, 1, d//2 -> B * C, M, d//2
+        ], dim=-1).flatten(0, 1).unsqueeze(1) # B * C, M, d -> B * C * M, d -> B * C * M, 1, d
 
-        dec_out = self.gru(dec_in, enc_out)[0]  # B0*C*M, D
+        dec_out = self.gru(dec_in, enc_out)[0]  # B * C * M, 1, d
 
-        yw = self.dropout(dec_out)
-        yd = self.linear_patch_re(yw)  # B * M, 1, D
-        pred = yd.reshape(B, -1, 1).squeeze(-1) # B, H
+        yd = self.dropout(dec_out)
+        yw = self.linear_patch_re(yd)  # B * C * M, 1, d -> B * C * M, 1, W
+        y = yw.reshape(B, C, -1).permute(0, 2, 1) # B, C, H
 
-        pred = pred.unsqueeze(1).reshape(B0, C, -1).permute(0, 2, 1)
+        y = y + seq_last
 
-        pred = pred + seq_last
-
-        return pred
+        return y
